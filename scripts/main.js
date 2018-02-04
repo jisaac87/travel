20c8b35fbf4a2bede1af928083676ab1bd1c92cc
jisaac87 <johndavidisaac@gmail.com>
2018-02-03 17:47:30 -0700

    first push

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+    console.log('hello world')
+
+    let aboutButton = document.getElementById('to-do');
+    let contentToChange = document.getElementById('next-slide');
+    console.log(aboutButton);
+
+    aboutButton.addEventListener('click', function(){
+  contentToChange.innerHTML = 
+  
+  `<div class="bi">
+    <div class="work">Hot destinations</div>
+    <div class
+  </div>`
+
+    });
